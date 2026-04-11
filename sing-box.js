0b3b48c60e6649649b0f1ae88b803780c5aad18b
King <wzmlove1121@gmail.com>
2026-04-11 20:19:09 +0800

    Remove outdated configurations and region-specific routing logic from sing-box scripts and JSON files.

--- sing-box.js
+++ sing-box.js
@@ -3,6 +3,16 @@ const compatible_outbound = {
   tag: 'COMPATIBLE',
   type: 'direct',
 }
+const groupRegexMap = {
+  hk: /港|hk|hongkong|hong kong|🇭🇰/i,
+  tw: /台|tw|taiwan|🇹🇼/i,
+  jp: /日本|jp|japan|🇯🇵/i,
+  sg: /新加坡|狮城|(?:^|[^a-z])sg(?:$|[^a-z])|singapore|🇸🇬/i,
+  kr: /首尔|韩|韓|(?:^|[^a-z])(?:kr|kor)(?:$|[^a-z])|korea|🇰🇷/i,
+  us: /美国|美|(?:^|[^a-z])(?:us|usa)(?:$|[^a-z])|united[\s_-]*states?|america|🇺🇸/i,
+  fiddler: /fiddler/i,
+}
+const otherAutoRegexList = Object.values(groupRegexMap)
 
 let compatible
 let config = JSON.parse($files[0])
@@ -21,25 +31,28 @@ config.outbounds.map(i => {
     i.outbounds.push(...getTags(proxies))
   }
   if (['hk', 'hk-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /港|hk|hongkong|hong kong|🇭🇰/i))
+    i.outbounds.push(...getTags(proxies, groupRegexMap.hk))
   }
   if (['tw', 'tw-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /台|tw|taiwan|🇹🇼/i))
+    i.outbounds.push(...getTags(proxies, groupRegexMap.tw))
   }
   if (['jp', 'jp-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /日本|jp|japan|🇯🇵/i))
+    i.outbounds.push(...getTags(proxies, groupRegexMap.jp))
   }
   if (['sg', 'sg-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /新加坡|狮城|(?:^|[^a-z])sg(?:$|[^a-z])|singapore|🇸🇬/i))
+    i.outbounds.push(...getTags(proxies, groupRegexMap.sg))
   }
   if (['kr', 'kr-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /首尔|韩|韓|(?:^|[^a-z])(?:kr|kor)(?:$|[^a-z])|korea|🇰🇷/i))
+    i.outbounds.push(...getTags(proxies, groupRegexMap.kr))
   }
   if (['us', 'us-auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /美国|美|(?:^|[^a-z])(?:us|usa)(?:$|[^a-z])|united[\s_-]*states?|america|🇺🇸/i))
+    i.outbounds.push(...getTags(proxies, groupRegexMap.us))
   }
   if (['fiddler'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /fiddler/i))
+    i.outbounds.push(...getTags(proxies, groupRegexMap.fiddler))
+  }
+  if (['other-auto'].includes(i.tag)) {
+    i.outbounds.push(...getOtherTags(proxies, otherAutoRegexList))
   }
 })
 
@@ -57,4 +70,8 @@ $content = JSON.stringify(config, null, 2)
 
 function getTags(proxies, regex) {
   return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies).map(p => p.tag)
+}
+
+function getOtherTags(proxies, regexList) {
+  return proxies.filter(p => !regexList.some(regex => regex.test(p.tag))).map(p => p.tag)
 }
